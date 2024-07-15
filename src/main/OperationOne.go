8848bf8a4e07deabcad08bb394e6ccb58f56f16a
pengwenhao <pengwenhao@aliyun.com>
2024-07-15 14:19:55 +0800

    fix:update OperationOne

--- src/main/OperationOne.go
+++ src/main/OperationOne.go
@@ -24,4 +24,20 @@ func main() {
 	fmt.Printf("第三行 -c的值为:%d\n", c)
 	fmt.Println()
 
+	c = a / b
+	fmt.Printf("第四行 -c的值为:%d\n", c)
+	fmt.Println()
+
+	c = a % b
+	fmt.Printf("第五行 -c 的值为:%d\n", c)
+	fmt.Println()
+
+	a++
+	fmt.Printf("第六行 -a的值为:%d\n", a)
+	fmt.Println()
+
+	a = 21
+	a--
+	fmt.Printf("第七行 -a的值为:%d\n", a)
+
 }
