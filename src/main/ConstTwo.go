95d5948b217c4e45c22bad9e2e9eb762656468aa
pengwenhao <pengwenhao@aliyun.com>
2024-07-15 14:06:26 +0800

    fix:update ConstTwo

--- src/main/ConstTwo.go
+++ src/main/ConstTwo.go
@@ -1,1 +1,21 @@
 package main
+
+import "fmt"
+
+func main() {
+
+	const (
+		a1 = iota
+		b1
+		c1
+		d1 = "ha"
+		e1
+		f1 = 100
+		g1
+		h1 = iota
+		i1
+	)
+
+	fmt.Println(a1, b1, c1, d1, e1, f1, g1, h1, i1)
+
+}
