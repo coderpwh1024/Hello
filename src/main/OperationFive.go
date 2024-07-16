438f9600d2c022ea8dfd7a8b952366797f00fc77
pengwenhao <pengwenhao@aliyun.com>
2024-07-16 22:59:16 +0800

    feat:init OperationFive

---
+++ src/main/OperationFive.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var a int = 4
+
+	var b int32
+
+	var c float32
+
+	var ptr *int
+
+	fmt.Printf("第 1 行 -a 变量类型 = %T\n", a)
+
+	fmt.Printf("第 2 行 -b 变量类型 = %T\n", b)
+
+	fmt.Printf("第 3 行 -c 变量类型 = %T\n", c)
+
+	ptr = &a
+
+	fmt.Printf("a 的值为 %d\n", a)
+	fmt.Printf("*ptr 为 %d\n", *ptr)
+
+}
