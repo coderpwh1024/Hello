65b3ed7e7a0d07420b8de5ab86148a4eca82aa1a
pengwenhao <pengwenhao@aliyun.com>
2024-07-17 22:03:04 +0800

    feat:init DomainThree

--- src/main/DomainThree.go
+++ src/main/DomainThree.go
@@ -2,19 +2,19 @@ package main
 
 import "fmt"
 
-var a int = 20
+var a2 int = 20
 
 func main() {
 
-	var a int = 10
+	var a2 int = 10
 
 	var b int = 20
 
 	var c int = 0
 
-	fmt.Printf("main() 函数中 a=%d\n", a)
+	fmt.Printf("main() 函数中 a=%d\n", a2)
 
-	c = sum(a, b)
+	c = sum(a2, b)
 
 	fmt.Printf("main() 函数中 c=%d\n", c)
 }
