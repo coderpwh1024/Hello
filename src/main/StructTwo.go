cc1fe7bf52b8e2a9d3bd7361b84b7bbc3d865e34
pengwenhao <pengwenhao@aliyun.com>
2024-07-18 22:39:20 +0800

    feat:init StructTwo

---
+++ src/main/StructTwo.go
@@ -0,0 +1,43 @@
+package main
+
+import "fmt"
+
+type Book struct {
+	title string
+
+	author string
+
+	subject string
+
+	book_id int
+}
+
+func main() {
+
+	var Book1 Book
+
+	var Book2 Book
+
+	Book1.title = "Go语言"
+	Book1.author = "www.google.com"
+	Book1.subject = "Go 语言教程"
+	Book1.book_id = 123456
+
+	Book2.title = "Java 语言"
+	Book2.author = "www.google.com"
+	Book2.subject = "Java 语言教程"
+	Book2.book_id = 23456
+
+	fmt.Printf("Book 1 title %s\n", Book1.title)
+	fmt.Printf("Book 1 author %s\n", Book1.author)
+	fmt.Printf("Book 1 subject %s\n", Book1.subject)
+	fmt.Printf("Book 1 book_id %d\n", Book1.book_id)
+
+	fmt.Println("----------------------------------------------------------------")
+
+	fmt.Printf("Book 2 title %s\n", Book2.title)
+	fmt.Printf("Book 2 author %s\n", Book2.author)
+	fmt.Printf("Book 2 subject %s\n", Book2.subject)
+	fmt.Printf("Book 2 book_id %d\n", Book2.book_id)
+
+}
