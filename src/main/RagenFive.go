1f3963f0ce1f750880adad4cf3a2d5b0534eb7a6
pengwenhao <pengwenhao@aliyun.com>
2024-07-23 14:04:22 +0800

    feat:init RangeFive

--- src/main/RagenFive.go
+++ src/main/RagenFive.go
@@ -13,5 +13,23 @@ func main() {
 	}
 
 	fmt.Println("sum:", sum)
+	fmt.Println("------------------------------------")
+	for i, num := range nums {
+		if num == 3 {
+			fmt.Println("index:", i)
+		}
+	}
+
+	kvs := map[string]string{"a": "apple", "b": "banana"}
+
+	for k, v := range kvs {
+		fmt.Printf("%s ->%s\n", k, v)
+	}
+
+	fmt.Println("------------------------------------")
+
+	for i, c := range "go" {
+		fmt.Println(i, c)
+	}
 
 }
