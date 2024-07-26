8f4badbacc6a7728f571dda18278fa0fc4a61e86
pengwenhao <pengwenhao@aliyun.com>
2024-07-26 13:43:13 +0800

    fix:update TypeTwo

--- src/main/TypeTwo.go
+++ src/main/TypeTwo.go
@@ -7,14 +7,20 @@ import (
 
 func main() {
 
-	str := "123"
+	/*	str := "123"
 
-	num, err := strconv.Atoi(str)
+		num, err := strconv.Atoi(str)
 
-	if err != nil {
-		fmt.Println("转换错误:", err)
-	} else {
-		fmt.Printf("字符串 '%s' 转换为整数为: %d\n", str, num)
-	}
+		if err != nil {
+			fmt.Println("转换错误:", err)
+		} else {
+			fmt.Printf("字符串 '%s' 转换为整数为: %d\n", str, num)
+		}
+	*/
+
+	num := 3.14
+	str := strconv.FormatFloat(num, 'f', 2, 64)
+
+	fmt.Printf("浮点数 %f 转为字符串为: '%s'\n", num, str)
 
 }
